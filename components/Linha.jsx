77b0024182f21a896204b163a915c33cbcf146ba
Vitor Farias <joaovitor1081@gmail.com>
2022-06-13 21:23:02 -0300

    linha DO X

---
+++ components/Linha.jsx
@@ -0,0 +1,15 @@
+import styles from '../styles/linha.module.css'
+import Subdivisao from "./Subdivisao";
+
+export default function Linha(props){
+    return (
+    <div className={styles.linha}>
+        <Subdivisao/>
+        <Subdivisao preta/>
+        <Subdivisao/>
+        <Subdivisao preta/>        <Subdivisao/>
+        <Subdivisao preta/>        <Subdivisao/>
+        <Subdivisao preta/>
+    </div>
+    )
+}
